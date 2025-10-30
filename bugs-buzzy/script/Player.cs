1d85eb78f3c466e30eff4a7318ad6839ea90a11b
Arman Ebrahimi Khoshdel <armanebrahimikhoshdel@gmail.com>
2025-10-30 17:40:47 +0330

    changed player

--- bugs-buzzy/script/Player.cs
+++ bugs-buzzy/script/Player.cs
@@ -1,131 +1,105 @@
 using Godot;
 
 
-public partial class Player : CharacterBody2D
+public partial class player : CharacterBody2D
 {
-	  [Export] public float Speed = 600.0f;
-	  [Export] public float JumpVelocity = -500.0f;
-	  [Export] public float wallJumpHorizantalVelocity = 200.0f;
-	  [Export] public int maxSoul = 100;
-	  private int soul;
-	  private AnimatedSprite2D animator;
-	  private bool isMoving = false;
-	  private Vector2 velocity;
-	  private int jumpCounter = 0;
+    [Export] public float Speed = 300.0f;
+    [Export] public float JumpVelocity = -250.0f;
+    [Export] public float wallJumpHorizantalVelocity = 300.0f;
+    private AnimatedSprite2D animator;
+    private bool isMoving = false;
+    private Vector2 velocity;
+    private int jumpCounter = 0;
+    private 
 
-	  private bool isWallJumping = false;
-	  
+    public override void _Ready()
+    {
+        animator = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+    }
 
-	  public override void _Ready()
-	  {
-		 animator = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-		 soul = maxSoul;
-	  }
-
-	  public override void _PhysicsProcess(double delta)
-	{
-		velocity = Velocity;
+    public override void _PhysicsProcess(double delta)
+    {
+        velocity = Velocity;
 
 		
 		
-		handleMovment();
-		jump(delta);
-		handleAnimation();
+        handleMovment();
+        jump(delta);
+        handleAnimation();
 
-		Velocity = velocity;
-		MoveAndSlide();
-	}
+        Velocity = velocity;
+        MoveAndSlide();
+    }
 
-	private void handleMovment()
-	{
-		Vector2 direction = Input.GetVector("moveLeft", "moveRight", "moveForward", "moveBackward");
-		if (isWallJumping != true)
-		{
-			if (direction != Vector2.Zero)
-			{
-				isMoving = true;
-				velocity.X = direction.X * Speed;
-				animator.Play("walk");
-			}
-			else
-			{
-				animator.Play("idle");
-				velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			}
-		}
-			
+    private void handleMovment()
+    {
+        Vector2 direction = Input.GetVector("moveLeft", "moveRight", "moveForward", "moveBackward");
+        if (direction != Vector2.Zero)
+        {
+            isMoving = true;
+            velocity.X = direction.X * Speed;
+            animator.Play("walk");
+        }
+        else
+        {
+            animator.Play("idle");
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+        }
 
-	}
-	private void jump(double delta)
-	{
-  
+    }
+    private void jump(double delta)
+    {
 
-		if (!IsOnFloor() )
-		{
-			if (!IsOnWall())
-			{
-				velocity += GetGravity() * (float)delta; 
-			}
-			else if(IsOnWall() && velocity.Y <= 0)
-			{
-				velocity += (GetGravity()/2) * (float)delta;  
-			}
-			
-		}
-		
+        if (!IsOnFloor())
+        {
+            velocity += GetGravity() * (float)delta;
+        }
 
-  
-		if (Input.IsActionJustPressed("ui_accept"))
-		{
-		   
-			if (IsOnFloor())
-			{
-			   
-				jumpCounter++;
-				velocity.Y = JumpVelocity;
-			}
-		 
-			else if (IsOnWall())
-			{
-				isWallJumping = true;
-				int wallDir = GetWallNormal().X > 0 ? 1 : -1;
-				velocity.Y = JumpVelocity;
-				velocity.X = wallDir * wallJumpHorizantalVelocity;
+        if (Input.IsActionJustPressed("ui_accept"))
+        {
 
-			 
-				jumpCounter = 1;
-			}
-		}
+            if (IsOnFloor())
+            {
+                jumpCounter++;
+                velocity.Y = JumpVelocity;
+            }
 
-		if (IsOnFloor())
-		{
-			isWallJumping = false;
-			jumpCounter = 0;
-		}
-	}
+            else if (IsOnWall())
+            {
+
+                int wallDir = GetWallNormal().X > 0 ? 1 : -1;
+
+                velocity.Y = JumpVelocity;
+                velocity.X = wallDir * wallJumpHorizantalVelocity;
+
+                jumpCounter = 1;
+            }
+        }
+
+        if (IsOnFloor())
+        {
+            jumpCounter = 0;
+        }
+    }
 
 
-	private void handleAnimation( )
-	{
-		if(velocity .Y > 0)
-		{
-			animator.Play("fall");
-		}else if (velocity.Y < 0)
-		{
-			animator.Play("jump");
-		}
-		if (velocity.X > 0)
-		{
-			animator.FlipH = false;
-		}
-		else if(velocity.X < 0)
-		{
-			animator.FlipH = true;
-		}
-	}
-
-	public void takeDame(float damage)
-	{
-		
-	}
+    private void handleAnimation( )
+    {
+        if(velocity .Y > 0)
+        {
+            animator.Play("fall");
+        }else if (velocity.Y < 0)
+        {
+            animator.Play("jump");
+        }
+        if (velocity.X > 0)
+        {
+            animator.FlipH = false;
+        }
+        else if(velocity.X < 0)
+        {
+            animator.FlipH = true;
+        }
+    }
 }
+
