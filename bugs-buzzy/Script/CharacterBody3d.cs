187e915462ccd8dc2ebdd53fbd4da88d56a734d6
Arman Ebrahimi Khoshdel <armanebrahimikhoshdel@gmail.com>
2025-10-30 10:51:22 +0330

    playerMovment

--- bugs-buzzy/Script/CharacterBody3d.cs
+++ bugs-buzzy/Script/CharacterBody3d.cs
@@ -3,41 +3,89 @@ using System;
 
 public partial class CharacterBody3d : CharacterBody3D
 {
-	public const float Speed = 5.0f;
-	public const float JumpVelocity = 4.5f;
+    private const float Speed = 5.0f;
+    private const float JumpVelocity = 4.5f;
 
-	public override void _PhysicsProcess(double delta)
-	{
-		Vector3 velocity = Velocity;
+    private float MouseSensitivity = 0.005f;
+    private float pitch = 0.0f;
+    private Camera3D camera;
 
-		// Add the gravity.
-		if (!IsOnFloor())
-		{
-			velocity += GetGravity() * (float)delta;
-		}
+    private int health;
+    private int maxHealth = 100;
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
+ 
+    private float damageCooldown = 1f;
+    private float damageTimer = 0f;
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		if (direction != Vector3.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
-		}
+    public override void _Ready()
+    {
+        health = maxHealth;
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+        camera = GetNode<Camera3D>("Camera3D");
+    }
 
-		Velocity = velocity;
-		MoveAndSlide();
-	}
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseMotion mouseMotion)
+        {
+            RotateY(-mouseMotion.Relative.X * MouseSensitivity);
+
+            pitch -= mouseMotion.Relative.Y * MouseSensitivity;
+            pitch = Mathf.Clamp(pitch, -1.2f, 1.2f);
+            camera.Rotation = new Vector3(pitch, 0, 0);
+        }
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+ 
+        if (damageTimer > 0)
+            damageTimer -= (float)delta;
+
+        Vector3 velocity = Velocity;
+
+        if (!IsOnFloor())
+            velocity += GetGravity() * (float)delta;
+
+        if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+            velocity.Y = JumpVelocity;
+
+        Vector2 inputDir = Input.GetVector("moveLeft", "moveRight", "moveForward", "moveBackward");
+        Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+
+        if (direction != Vector3.Zero)
+        {
+            velocity.X = direction.X * Speed;
+            velocity.Z = direction.Z * Speed;
+        }
+        else
+        {
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+            velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+        }
+
+        Velocity = velocity;
+        MoveAndSlide();
+    }
+
+  
+    public void TakeDamage(int amount)
+    {
+        if (damageTimer > 0) 
+            return; 
+
+        health -= amount;
+        GD.Print("Player took damage: ", amount, " | Health: ", health);
+
+        damageTimer = damageCooldown; 
+
+        if (health <= 0)
+            Die();
+    }
+
+    private void Die()
+    {
+        GD.Print("You are dead");
+      
+    }
 }
