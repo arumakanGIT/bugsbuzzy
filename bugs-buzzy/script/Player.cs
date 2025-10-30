93fcab67fcaf8699b81cb056edfc2dd9ebaa2399
Arman Ebrahimi Khoshdel <armanebrahimikhoshdel@gmail.com>
2025-10-30 12:02:05 +0330

    added anim for player

--- bugs-buzzy/script/Player.cs
+++ bugs-buzzy/script/Player.cs
@@ -3,14 +3,21 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	  [Export] public float Speed = 600.0f;
+	  [Export] public float JumpVelocity = -500.0f;
+	  private AnimatedSprite2D animator;
+	  private bool isMoving = false;
 
-	public override void _PhysicsProcess(double delta)
+	  public override void _Ready()
+	  {
+		 animator = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	  }
+
+	  public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
-
+		
 		if (!IsOnFloor())
 		{
 			velocity += GetGravity() * (float)delta;
@@ -21,15 +28,29 @@ public partial class Player : CharacterBody2D
 			velocity.Y = JumpVelocity;
 		}
 
+		
+	
 
 		Vector2 direction = Input.GetVector("moveLeft", "moveRight", "moveForward", "moveBackward");
 		if (direction != Vector2.Zero)
 		{
+			isMoving = true;
 			velocity.X = direction.X * Speed;
+			animator.Play("walk");
 		}
 		else
 		{
+			animator.Play("idle");
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		}
+
+		if (velocity.X > 0)
+		{
+			animator.FlipH = false;
+		}
+		else if(velocity.X < 0)
+		{
+			animator.FlipH = true;
 		}
 
 		Velocity = velocity;
