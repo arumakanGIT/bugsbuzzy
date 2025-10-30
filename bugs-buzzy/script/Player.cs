4f64f061ab5700044437589e697bb783938650c3
Arman Ebrahimi Khoshdel <armanebrahimikhoshdel@gmail.com>
2025-10-30 11:31:30 +0330

    added sprites

--- bugs-buzzy/script/Player.cs
+++ bugs-buzzy/script/Player.cs
@@ -10,21 +10,19 @@ public partial class Player : CharacterBody2D
 	{
 		Vector2 velocity = Velocity;
 
-		// Add the gravity.
+
 		if (!IsOnFloor())
 		{
 			velocity += GetGravity() * (float)delta;
 		}
 
-		// Handle Jump.
 		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
 		{
 			velocity.Y = JumpVelocity;
 		}
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+
+		Vector2 direction = Input.GetVector("moveLeft", "moveRight", "moveForward", "moveBackward");
 		if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * Speed;
